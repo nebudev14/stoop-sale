5460e8338bf00f554ea26859e0f01e9b7b3ab8aa
Serena Yung <serenayung2005@gmail.com>
2024-07-11 22:38:00 -0400

    balls

---
+++ components/EnvScroll.js
@@ -0,0 +1,26 @@
+import { motion, Varients, useScroll, scroll } from "framer-motion"
+
+const EnvScroll = () => {
+
+    return(
+        <motion.img className="w-9/12 h-9/12" src="http://localhost:3000/env%201.png"
+            
+            initial={{scale: 1, y:100}}
+            whileInView={{
+                y: 0,
+                transition: {
+                    type: "spring",
+                    bounce: 0.4,
+                    duration: 0.8
+                },
+                viewport: {margin: "some"}
+            }
+            }
+            
+
+        >
+
+        </motion.img>
+    );
+}
+export default EnvScroll;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Catalog from "@/components/Catalog";
-// import { motion, Varients } from "framer-motion"
+import EnvScroll from "@/components/EnvScroll";
+import { motion, Varients } from "framer-motion"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,11 +13,20 @@ export default function Home() {
     <main
       className={`min-h-screen flex-col justify-between p-16 ${inter.className}`}
     >
-      <video className="w-screen h-screen" muted>
-        <source src="http://localhost:3000/stoop-sale-landing.mp4" type="video/mp4" />
-      </video>
+      <div>
+        <video className="w-screen h-screen" autoPlay muted loop>
+          <source src="http://localhost:3000/stoop-sale-landing.mp4" type="video/mp4" />
+        </video>
 
-      <button className="px-12 py-2 text-6xl text-white rounded-md bg-stoop-green font-old hover:bg-[#9BAC97] hover:ring-green-900 ml-auto">RSVP</button>
+        <EnvScroll />
+      </div>
+     
+
+      <div className="justify-center grid my-10">
+        <button className="px-12 py-2 text-6xl text-white rounded-md bg-stoop-green font-old hover:bg-[#9BAC97] hover:ring-green-900 mb-2">RSVP</button>
+        <span>&#128205; Carroll Gardens (Court Street & 2nd Pl, Brooklyn)</span>
+      </div>
+      
 
       <Navbar className="flex items-center"/>
       <Catalog />
