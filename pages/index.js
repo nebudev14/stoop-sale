96ec0188733051d69b2fb58d5987c9265e4426a6
warren yun <wyun2006@gmail.com>
2024-07-11 22:46:34 -0400

    video format

--- pages/index.js
+++ pages/index.js
@@ -10,26 +10,30 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`min-h-screen flex-col justify-between p-16 ${inter.className}`}
-    >
-      <div>
-        <video className="w-screen h-screen" autoPlay muted loop>
-          <source src="http://localhost:3000/stoop-sale-landing.mp4" type="video/mp4" />
-        </video>
+    <>
 
-        <EnvScroll />
-      </div>
-     
+      <video className="object-fill w-screen h-screen" autoPlay muted loop>
+        <source src="http://localhost:3000/stoop-sale-landing.mp4" className="w-screen" type="video/mp4" />
+      </video>
 
-      <div className="justify-center grid my-10">
-        <button className="px-12 py-2 text-6xl text-white rounded-md bg-stoop-green font-old hover:bg-[#9BAC97] hover:ring-green-900 mb-2">RSVP</button>
-        <span>&#128205; Carroll Gardens (Court Street & 2nd Pl, Brooklyn)</span>
-      </div>
-      
+      <main
+        className={`min-h-screen flex-col justify-between p-16 ${inter.className}`}
+      >
+        <div>
 
-      <Navbar className="flex items-center"/>
-      <Catalog />
-    </main>
+          <EnvScroll />
+        </div>
+
+
+        <div className="grid justify-center my-10">
+          <button className="px-12 py-2 text-6xl text-white rounded-md bg-stoop-green font-old hover:bg-[#9BAC97] hover:ring-green-900 mb-2">RSVP</button>
+          <span>&#128205; Carroll Gardens (Court Street & 2nd Pl, Brooklyn)</span>
+        </div>
+
+
+        <Navbar className="flex items-center" />
+        <Catalog />
+      </main>
+    </>
   );
 }
